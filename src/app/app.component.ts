1dd6de50d88efb2e198f2752b64ae06c4b264f35
LAPTOP-JHLSCDA7\steve <steve@soaconsultingservices.com>
2020-09-22 20:22:47 -0400

    Angular 10 Hellow World

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class AppComponent {
-  title = 'hellowworld';
+  name: string;
+  title: string;
+
+  constructor() {
+    this.name = 'World - Angular 10.1.2 is really fun!';
+    this.title = 'HelloWorld';
+  }
 }
